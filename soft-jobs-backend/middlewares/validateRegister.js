61f3d9bf99c573d881b8429e69d62e624043c4ac
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-01-25 12:02:27 -0300

    modify validation back and front

--- soft-jobs-backend/middlewares/validateRegister.js
+++ soft-jobs-backend/middlewares/validateRegister.js
@@ -1,7 +1,20 @@
 export const validateRegister = (req, res, next) => {
     const { email, password, role, lenguage } = req.body;
-    if (!email || !password || !role || !lenguage) {
-        return res.status(400).json({ message: "You need to enter all data" });
+    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!email || !emailRegex.test(email)) {
+        return res
+            .status(400)
+            .json({ message: "You need to enter a valid email" });
+    }
+    const passwordRegex = /^.{4,}$/;
+    if (!password || !passwordRegex.test(password)) {
+        return res.status(400).json({
+            message: "The password must be at least 4 characters long",
+        });
+    } else if (!role || !lenguage) {
+        return res.status(400).json({
+            message: "You need to choose a role and a lenguage",
+        });
     }
     next();
 };
--- soft-jobs-frontend/src/views/Register.jsx
+++ soft-jobs-frontend/src/views/Register.jsx
@@ -1,112 +1,121 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { ENDPOINT } from '../config/constans'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { ENDPOINT } from "../config/constans";
 
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+const passwordRegex = /^.{4,}$/;
 const initialForm = {
-  email: 'docente@desafiolatam.com',
-  password: '123456',
-  role: 'Select a role',
-  lenguage: 'Select a lenguage'
-}
+    email: "docente@desafiolatam.com",
+    password: "123456",
+    role: "Select a role",
+    lenguage: "Select a lenguage",
+};
 
 const Register = () => {
-  const navigate = useNavigate()
-  const [user, setUser] = useState(initialForm)
+    const navigate = useNavigate();
+    const [user, setUser] = useState(initialForm);
 
-  const handleUser = (event) => setUser({ ...user, [event.target.name]: event.target.value })
+    const handleUser = (event) =>
+        setUser({ ...user, [event.target.name]: event.target.value });
 
-  const handleForm = (event) => {
-    event.preventDefault()
+    const handleForm = (event) => {
+        event.preventDefault();
 
-    if (
-      !user.email.trim() ||
-      !user.password.trim() ||
-      user.rol === 'Select a role' ||
-      user.lenguage === 'Select a lenguage'
-    ) {
-      return window.alert('All field are required.')
-    }
+        if (
+            !user.email.trim() ||
+            !user.password.trim() ||
+            user.role === "Select a role" ||
+            user.lenguage === "Select a lenguage"
+        ) {
+            return window.alert("All field are required.");
+        } else if (!emailRegex.test(user.email)) {
+            return window.alert("Email format is not correct!");
+        } else if (!passwordRegex.test(user.password)) {
+            return window.alert("Password must have at least 4 characters!");
+        }
 
-    if (!emailRegex.test(user.email)) {
-      return window.alert('Email format is not correct!')
-    }
+        axios
+            .post(ENDPOINT.users, user)
+            .then(() => {
+                window.alert("User registered successfully 😀.");
+                navigate("/login");
+            })
+            .catch(({ response: { data } }) => {
+                console.error(data);
+                window.alert(`${data.message} 🙁.`);
+            });
+    };
 
-    axios.post(ENDPOINT.users, user)
-      .then(() => {
-        window.alert('User registered successfully 😀.')
-        navigate('/login')
-      })
-      .catch(({ response: { data } }) => {
-        console.error(data)
-        window.alert(`${data.message} 🙁.`)
-      })
-  }
+    useEffect(() => {
+        if (window.sessionStorage.getItem("token")) {
+            navigate("/profile");
+        }
+    }, []);
 
-  useEffect(() => {
-    if (window.sessionStorage.getItem('token')) {
-      navigate('/profile')
-    }
-  }, [])
+    return (
+        <form
+            onSubmit={handleForm}
+            className="col-10 col-sm-6 col-md-3 m-auto mt-5">
+            <h1>Register a new user</h1>
+            <hr />
+            <div className="form-group mt-1 ">
+                <label>Email address</label>
+                <input
+                    value={user.email}
+                    onChange={handleUser}
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="Enter email"
+                />
+            </div>
+            <div className="form-group mt-1 ">
+                <label>Password</label>
+                <input
+                    value={user.password}
+                    onChange={handleUser}
+                    type="password"
+                    name="password"
+                    className="form-control"
+                    placeholder="Password"
+                />
+            </div>
+            <div className="form-group mt-1 ">
+                <label>Role</label>
+                <select
+                    defaultValue={user.role}
+                    onChange={handleUser}
+                    name="role"
+                    className="form-select">
+                    <option disabled>Select a role</option>
+                    <option value="Full Stack Developer">
+                        Full Stack Developer
+                    </option>
+                    <option value="Frontend Developer">
+                        Frontend Developer
+                    </option>
+                    <option value="Backend Developer">Backend Developer</option>
+                </select>
+            </div>
+            <div className="form-group mt-1">
+                <label>Lenguage</label>
+                <select
+                    defaultValue={user.lenguage}
+                    onChange={handleUser}
+                    name="lenguage"
+                    className="form-select">
+                    <option disabled>Select a lenguage</option>
+                    <option value="JavaScript">JavaScript</option>
+                    <option value="Python">Python</option>
+                    <option value="Ruby">Ruby</option>
+                </select>
+            </div>
+            <button type="submit" className="btn btn-light mt-3">
+                Register
+            </button>
+        </form>
+    );
+};
 
-  return (
-    <form onSubmit={handleForm} className='col-10 col-sm-6 col-md-3 m-auto mt-5'>
-      <h1>Register a new user</h1>
-      <hr />
-      <div className='form-group mt-1 '>
-        <label>Email address</label>
-        <input
-          value={user.email}
-          onChange={handleUser}
-          type='email'
-          name='email'
-          className='form-control'
-          placeholder='Enter email'
-        />
-      </div>
-      <div className='form-group mt-1 '>
-        <label>Password</label>
-        <input
-          value={user.password}
-          onChange={handleUser}
-          type='password'
-          name='password'
-          className='form-control'
-          placeholder='Password'
-        />
-      </div>
-      <div className='form-group mt-1 '>
-        <label>Role</label>
-        <select
-          defaultValue={user.role}
-          onChange={handleUser}
-          name='role'
-          className='form-select'
-        >
-          <option disabled>Select a role</option>
-          <option value='Full Stack Developer'>Full Stack Developer</option>
-          <option value='Frontend Developer'>Frontend Developer</option>
-          <option value='Backend Developer'>Backend Developer</option>
-        </select>
-      </div>
-      <div className='form-group mt-1'>
-        <label>Lenguage</label>
-        <select
-          defaultValue={user.lenguage}
-          onChange={handleUser}
-          name='lenguage'
-          className='form-select'
-        >
-          <option disabled>Select a lenguage</option>
-          <option value='JavaScript'>JavaScript</option>
-          <option value='Python'>Python</option>
-          <option value='Ruby'>Ruby</option>
-        </select>
-      </div>
-      <button type='submit' className='btn btn-light mt-3'>Register</button>
-    </form>
-  )
-}
-
-export default Register
+export default Register;
